fd0fa7ab54093462295733fdc9202f8e210fd26f
Natdenice <Clad1987@hotmail.com>
2019-03-15 18:45:48 +0100

    Updating the budget controller first part done

--- app.js
+++ app.js
@@ -130,9 +130,15 @@ var budgetController = (function () {
       // return the new element
       return newItem;
     
+    },
+    
+
+    testing: function() {
+      console.log(data);
     }
-  
   };
+
+    
 
 })();
 
@@ -159,7 +165,7 @@ var UIController = (function () {
       return {
         type: document.querySelector(DOMstrings.inputType).value, // Will be either 'inc' or 'exp'
         description: document.querySelector(DOMstrings.inputDescription).value,
-        value: document.querySelector(DOMstrings.inputValue).value
+        value: parseFloat(document.querySelector(DOMstrings.inputValue).value)
       };
     },
 
@@ -273,7 +279,20 @@ var controller = (function(budgetCtrl, UICtrl) {
     });
   };
 
+  var updateBudget = function () {
 
+  // 1 Calculate the budget
+
+  // 2 Return the budget
+
+  // 3 Display the budget on the UI
+
+
+
+
+
+  };
+  
   var ctrlAddItem = function () {
     
     var input, newItem;
@@ -282,7 +301,8 @@ var controller = (function(budgetCtrl, UICtrl) {
 
     input = UICtrl.getinput();
 
-    // 2 Add the item to the budget controller
+    if (input.description !== '' && !isNaN(input.value) && input.value > 0) {
+      // 2 Add the item to the budget controller
 
     newItem = budgetCtrl.addItem(input.type, input.description, input.value);
 
@@ -295,13 +315,17 @@ var controller = (function(budgetCtrl, UICtrl) {
     
     UICtrl.clearFields();
 
-    // 5 Calculate the budget
-
-
-    // 6 Display the budget on the UI
-
+    // 5 Calculate and update Budget
+    updateBudget();
   };
 
+
+
+
+
+    }
+
+    
   return {
     init: function () {
       console.log('The application has started !');
