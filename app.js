8f4c183c36e1f458fcb3722cce4cd3a751310c6c
Natdenice <Clad1987@hotmail.com>
2019-03-15 09:36:15 +0100

    clearFields function added and applied

--- app.js
+++ app.js
@@ -186,6 +186,51 @@ var UIController = (function () {
       document.querySelector(element).insertAdjacentHTML('beforeend', newHtml);
     },
 
+    clearFields: function (){ 
+      
+      var fields, fieldsArr; 
+      
+      fields = document.querySelectorAll(DOMstrings.inputDescription + ', ' + DOMstrings.inputValue);
+      
+      // this returns a list , not an array. So in order to transform the list into array 
+      // (and then loop through this array)
+      // we need to define a new var called fieldsArr and build a prototype with the slice method ,
+      // and then call method
+      // slice return a copy of the array , so it will return a copy of the array - 
+      // then we pass the list (called fields variable) to the call method.
+      // we cannot use only slice method , and pass fields variable with the slice method , cause
+      // fields is not an array it is just a list. We have to use the Array
+
+      /* Where is the slice method actually stored
+         that we can use it? Let's think about that. So where do you think it is stored?
+         And the solution is, that it's in the array prototype.
+         So we can write something like this. This array is the function constructor
+         for all arrays, right? And we know that all the methods
+         that the arrays inherit from the array function constructors
+         are in the array's prototype property, right? And therefore, we know the slice method must also be there.
+         So we can write "array dot prototype" and then "dot slice".
+         And since this is a function,
+         we can then use the call method on it, right?
+         So, "call". And then in here, we setted this variable to the fields.
+         And this will trick the slice method into thinking
+         that we give it an array, and so it will return an array.
+         So if we now store this into a variable and call it fields array
+         then this will be an array. So, fields array.
+         This is now an array. This is a nice little trick that we can use.
+         This means that we can now loop over this array
+         and clear all the fields that were selected */
+
+        fieldsArr = Array.prototype.slice.call(fields);
+
+        fieldsArr.forEach(function(current, index, array) {
+          current.value = "";
+
+        });
+    
+        fieldsArr[0].focus();
+
+    },
+
     getDOMstrings: function() {
       return DOMstrings;
     } 
@@ -246,11 +291,14 @@ var controller = (function(budgetCtrl, UICtrl) {
     
     UICtrl.addListItem(newItem, input.type);
 
+    // 4 Clear the fields
+    
+    UICtrl.clearFields();
 
-    // 4 Calculate the budget
+    // 5 Calculate the budget
 
 
-    // 5 Display the budget on the UI
+    // 6 Display the budget on the UI
 
   };
 
